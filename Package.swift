71e53c731a4c42e5ceee692605d87545b145750f
Chandra Poluru <poluruc@gmail.com>
2025-09-02 10:09:03 -0400

    Refactor Package.swift to use standard library names instead of binary suffixes

--- Package.swift
+++ Package.swift
@@ -7,41 +7,41 @@ let package = Package(
         .iOS(.v16)
     ],
     products: [
-        .library(name: "AlamofireBinary", targets: ["AlamofireBinary"]),
-        .library(name: "JWTDecodeBinary", targets: ["JWTDecodeBinary"]),
-        .library(name: "KingfisherBinary", targets: ["KingfisherBinary"]),
-        .library(name: "RealmBinary", targets: ["RealmBinary"]),
-        .library(name: "RealmSwiftBinary", targets: ["RealmSwiftBinary"]),
-        .library(name: "DGChartsBinary", targets: ["DGChartsBinary"])
+        .library(name: "Alamofire", targets: ["Alamofire"]),
+        .library(name: "JWTDecode", targets: ["JWTDecode"]),
+        .library(name: "Kingfisher", targets: ["Kingfisher"]),
+        .library(name: "Realm", targets: ["Realm"]),
+        .library(name: "RealmSwift", targets: ["RealmSwift"]),
+        .library(name: "DGCharts", targets: ["DGCharts"])
     ],
     targets: [
         .binaryTarget(
-            name: "AlamofireBinary",
+            name: "Alamofire",
             url: "https://github.com/poluruc/ios-frameworks/raw/refs/heads/main/Alamofire.xcframework.zip",
             checksum: "f1b28c3fc7f0e812f5d7b099fbc2007dfa698bd35858e490bc7d4faf98694fa9"
         ),
         .binaryTarget(
-            name: "JWTDecodeBinary",
+            name: "JWTDecode",
             url: "https://github.com/poluruc/ios-frameworks/raw/refs/heads/main/JWTDecode.xcframework.zip",
             checksum: "8d2cc76327adbd777a1a79041d16b51bd0193cef3b8f77bb3f5d202234361c68"
         ),
         .binaryTarget(
-            name: "KingfisherBinary",
+            name: "Kingfisher",
             url: "https://github.com/poluruc/ios-frameworks/raw/refs/heads/main/Kingfisher.xcframework.zip",
             checksum: "a1622497a3327a4726127f9976c36378e7b79c3d0cc2dd8c4f2836bb016cb4fc"
         ),
         .binaryTarget(
-            name: "RealmBinary",
+            name: "Realm",
             url: "https://github.com/poluruc/ios-frameworks/raw/refs/heads/main/Realm.xcframework.zip",
             checksum: "ef021fa9d65fca9829a3de73ca714fd0185b8c50dad413e967d3494b1a665b34"
         ),
         .binaryTarget(
-            name: "RealmSwiftBinary",
+            name: "RealmSwift",
             url: "https://github.com/poluruc/ios-frameworks/raw/refs/heads/main/RealmSwift.xcframework.zip",
             checksum: "5a60f71644799f1fa14961d55872c07ce43a5344cd7195f0abde78d9ae90f09e"
         ),
         .binaryTarget(
-            name: "DGChartsBinary",
+            name: "DGCharts",
             url: "https://github.com/poluruc/ios-frameworks/raw/refs/heads/main/DGCharts.xcframework.zip",
             checksum: "de699c79f032bdf089131cee3d8b92cc9edcbde918d524a687380d056afdad71"
         )
