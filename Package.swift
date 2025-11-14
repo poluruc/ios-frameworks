f43856007a9c339ba868dc6fd202262163f0cb50
Chandra Poluru <chandra.poluru@telus.com>
2025-11-14 13:03:05 -0500

    Update platform to iOS 14

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "ios-frameworks",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v16)
     ],
     products: [
         .library(name: "Alamofire", targets: ["Alamofire"]),
